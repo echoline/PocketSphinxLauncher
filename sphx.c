90c6bd11f8a860af9189859071999047aa87dbc1
Linux User <eli@singularity.my.domain>
2019-06-28 20:07:14 -0700

    !@?

--- sphx.c
+++ sphx.c
@@ -74,7 +74,6 @@
 
 #include <sphinxbase/err.h>
 #include <sphinxbase/ad.h>
-#include <sphinxbase/cont_ad.h>
 
 #include <pocketsphinx.h>
 
@@ -87,7 +86,6 @@ extern char *lmdict;
 static ps_decoder_t *ps;
 static cmd_ln_t *config;
 static ad_rec_t *ad;
-static cont_ad_t *cont;
 
 /* Sleep for specified msec */
 static void
@@ -122,90 +120,50 @@ recognize_from_microphone(int outfd)
     char const *hyp;
     char const *uttid;
     char word[4096];
+    uint8 in_speech, utt_started;
 
     if ((ad = ad_open_dev(NULL,
                           (int)cmd_ln_float32_r(config, "-adcdev"))) == NULL)
         E_FATAL("Failed to open audio device\n");
 
-    /* Initialize continuous listening module */
-    if ((cont = cont_ad_init(ad, ad_read)) == NULL)
-        E_FATAL("Failed to initialize voice activity detection\n");
     if (ad_start_rec(ad) < 0)
         E_FATAL("Failed to start recording\n");
-    if (cont_ad_calib(cont) < 0)
-        E_FATAL("Failed to calibrate voice activity detection\n");
+    if (ps_start_utt(ps, NULL) < 0)
+        E_FATAL("Failed to start utterance\n");
+
+    utt_started = FALSE;
+    /* Indicate listening for next utterance */
+    write(outfd, "READY\n", 6);
 
     for (;;) {
-        /* Indicate listening for next utterance */
-	write(outfd, "READY\n", 6);
-
         /* Wait data for next utterance */
-        while ((k = cont_ad_read(cont, adbuf, 4096)) == 0)
+        while ((k = ad_read(ad, adbuf, 2048)) == 0)
             sleep_msec(100);
 
         if (k < 0)
             E_FATAL("Failed to read audio\n");
-
-        /*
-         * Non-zero amount of data received; start recognition of new utterance.
-         * NULL argument to uttproc_begin_utt => automatic generation of utterance-id.
-         */
-        if (ps_start_utt(ps, NULL) < 0)
-            E_FATAL("Failed to start utterance\n");
         ps_process_raw(ps, adbuf, k, FALSE, FALSE);
-	write(outfd, "Listening\n", 10);
-
-        /* Note timestamp for this first block of data */
-        ts = cont->read_ts;
-
-        /* Decode utterance until end (marked by a "long" silence, >1sec) */
-        for (;;) {
-            /* Read non-silence audio data, if any, from continuous listening module */
-            if ((k = cont_ad_read(cont, adbuf, 4096)) < 0)
-                E_FATAL("Failed to read audio\n");
-            if (k == 0) {
-                /*
-                 * No speech data available; check current timestamp with most recent
-                 * speech to see if more than 1 sec elapsed.  If so, end of utterance.
-                 */
-                if ((cont->read_ts - ts) > DEFAULT_SAMPLES_PER_SEC)
-                    break;
-            }
-            else {
-                /* New speech data received; note current timestamp */
-                ts = cont->read_ts;
-            }
-
-            /*
-             * Decode whatever data was read above.
-             */
-            rem = ps_process_raw(ps, adbuf, k, FALSE, FALSE);
-
-            /* If no work to be done, sleep a bit */
-            if ((rem == 0) && (k == 0))
-                sleep_msec(20);
+	in_speech = ps_get_in_speech(ps);
+       	if (in_speech && !utt_started) {
+            utt_started = TRUE;
+	    write(outfd, "Listening\n", 10);
         }
+	if (!in_speech && utt_started) {
+            /* speech -> silence transition, time to start new utterance  */
+            ps_end_utt(ps);
+            hyp = ps_get_hyp(ps, NULL );
+            if (hyp != NULL) {
+                dprintf(outfd, "%s\n", hyp);
+                fflush(stdout);
+            }
 
-        /*
-         * Utterance ended; flush any accumulated, unprocessed A/D data and stop
-         * listening until current utterance completely decoded
-         */
-        ad_stop_rec(ad);
-        while (ad_read(ad, adbuf, 4096) >= 0);
-        cont_ad_reset(cont);
-
-        /* Finish decoding, obtain and print result */
-        ps_end_utt(ps);
-        hyp = ps_get_hyp(ps, NULL, &uttid);
-        snprintf(word, sizeof(word)-1, "%s: %s\n", uttid, hyp);
-	write(outfd, word, strlen(word));
-
-        /* Resume A/D recording for next utterance */
-        if (ad_start_rec(ad) < 0)
-            E_FATAL("Failed to start recording\n");
+            if (ps_start_utt(ps) < 0)
+                E_FATAL("Failed to start utterance\n");
+            utt_started = FALSE;
+	    write(outfd, "READY\n", 10);
+        }
+        sleep_msec(100);
     }
-
-    cont_ad_close(cont);
     ad_close(ad);
 
     fprintf (stderr, "listen thread exiting\n");
